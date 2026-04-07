0cb9572ade4d2bb0bcbf594d89c5eda5c99cd07c
Resh_nbs <61882829+ReshmiSeetha@users.noreply.github.com>
2026-04-07 20:14:57 +0530

    Add files via upload

---
+++ RestAPI/RestApiExample.java
@@ -0,0 +1,56 @@
+package com.restApi;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class RestApiExample {
+	public static void main(String[] args) throws Exception {
+		
+		Transcript transcript = new Transcript();
+		List<String> speechModels = new ArrayList<>();
+		speechModels.add("universal-2");
+		transcript.setAudio_url("https://assembly.ai/wildfires.mp3");
+		transcript.setSpeech_models(speechModels);
+		
+		//Transcript to JSON
+		Gson gson = new Gson();
+		String jsonRequest = gson.toJson(transcript);
+		System.out.println(jsonRequest);
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(new java.net.URI("https://api.assemblyai.com/v2/transcript"))
+				.header("Authorization",API_KEY)
+				.POST(BodyPublishers.ofString(jsonRequest))
+				.build();
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		System.out.println(response.body());
+		
+		transcript=gson.fromJson(response.body(), Transcript.class);
+		System.out.println(transcript.getId());
+		
+		HttpRequest getRequest = HttpRequest.newBuilder()
+				.uri(new java.net.URI("https://api.assemblyai.com/v2/transcript/"+transcript.getId()))
+				.header("Authorization",API_KEY)
+				.GET()
+				.build();
+		while(true) {
+			HttpResponse<String> getResponse = client.send(getRequest, BodyHandlers.ofString());
+			transcript=gson.fromJson(getResponse.body(), Transcript.class);
+			System.out.println(transcript.getStatus());
+			if("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+				break;
+			}
+			Thread.sleep(5000);
+		}
+		System.out.println("Done");
+		System.out.println(transcript.getText());
+	}
+}
---
+++ RestAPI/Transcript.java
@@ -0,0 +1,53 @@
+package com.restApi;
+
+import java.util.List;
+
+public class Transcript {
+	
+	private String audio_url;
+	private List<String> speech_models;
+	private String id;
+	private String status;
+	private String text;
+	
+
+	public String getAudio_url() {
+		return audio_url;
+	}
+
+	public void setAudio_url(String audio_url) {
+		this.audio_url = audio_url;
+	}
+
+	public List<String> getSpeech_models() {
+		return speech_models;
+	}
+
+	public void setSpeech_models(List<String> speech_models) {
+		this.speech_models = speech_models;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
